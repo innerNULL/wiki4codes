a338f3fb9d4781f107d601c43913881e5eb29fdd
innerNULL <lyt697@outlook.com>
2021-10-18 07:28:52 +0000

    backup

---
+++ wiki4codes/lib_and_frameworks/redis-py-cluster/README.md
@@ -0,0 +1,11 @@
+## How to Start Redis-Cluster Locally
+```bash
+cd redis/utils/create-cluster
+./create-cluster start
+./create-cluster create
+```
+
+## Run Demo
+```bash
+${PYTHON_RUNTIME} ./demo.py
+```
---
+++ wiki4codes/lib_and_frameworks/redis-py-cluster/demo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+from rediscluster import RedisCluster
+
+startup_nodes = [
+        {'host':'localhost','port':'30001'},
+        {'host':'localhost','port':'30002'}, 
+        {'host':'localhost','port':'30003'}, 
+        {'host':'localhost','port':'30004'},
+        {'host':'localhost','port':'30005'},
+        {'host':'localhost','port':'30006'}
+
+]
+
+
+if __name__ == "__main__":
+    rc = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+    rc.set("foo", "bar0")
+    print(rc.get("foo"))
+    rc.set("foo", "bar1") 
+    print(rc.get("foo")) 
