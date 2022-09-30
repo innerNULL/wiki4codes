71c86c477a489d4a7b024f6973850f419113fec7
inull <10429190+innerNULL@users.noreply.github.com>
2022-09-30 05:10:53 +0000

    2022Q3
    feat: Adds a ccapi ticks data subscriber example.
     dev: Try not assign ccapi exchange in CMakeLists.

---
+++ lib_and_frameworks/crypto-chassis/ccapi/subscriber_demo.cpp
@@ -0,0 +1,50 @@
+/// file: subscriber_demo_v0.cpp 
+/// date: 2022-09-30
+
+
+#include <ccapi_cpp/ccapi_session.h>
+
+
+#define CCAPI_ENABLE_SERVICE_MARKET_DATA
+#define CCAPI_ENABLE_EXCHANGE_COINBASE
+
+
+
+class CustomizedEventHandler : public ccapi::EventHandler {
+ public:
+  bool processEvent(
+      const ccapi::Event& event, ccapi::Session* session
+  ) override {
+    if (event.getType() == ccapi::Event::Type::SUBSCRIPTION_DATA) {
+      for (const auto& message : event.getMessageList()) {
+        std::cout << std::string("Best bid and ask at ") 
+          << ccapi::UtilTime::getISOTimestamp(message.getTime()) 
+          << " are:" << std::endl;
+
+        for (const auto& element : message.getElementList()) {
+          const std::map<std::string, std::string>& 
+          elementNameValueMap = element.getNameValueMap();
+          std::cout << "  " + ccapi::toString(elementNameValueMap) << std::endl;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+
+int main(int argc, char** argv) {
+  std::cout << "Starting" << std::endl;
+
+  ccapi::SessionOptions sessionOptions;
+  ccapi::SessionConfigs sessionConfigs;
+  CustomizedEventHandler eventHandler;
+  ccapi::Session session(sessionOptions, sessionConfigs, &eventHandler);
+  ccapi::Subscription subscription("coinbase", "BTC-USD", "MARKET_DEPTH");
+  session.subscribe(subscription);
+  std::this_thread::sleep_for(std::chrono::seconds(100));
+  
+  session.stop();
+  std::cout << "End" << std::endl;
+  return 0;
+}
